9c9666e9cfdc8d3b885ab4c75d490d5cc4d8d7a0
Christian Trutz <christian.trutz@belaso.de>
2012-05-23 21:18:05 +0200

    Copyright owner is belaso.de now.
    Signed-off-by: Christian Trutz <christian.trutz@belaso.de>

---
+++ plugins/de.belaso.mongolyn.ui/src/de/belaso/mongolyn/ui/MongolynUtils.java
@@ -0,0 +1,18 @@
+/* 
+   Copyright (C) 2011, Christian Trutz <christian.trutz@belaso.de>
+
+   All rights reserved. This program and the accompanying materials
+   are made available under the terms of the Eclipse Public License v1.0
+   which accompanies this distribution, and is available at
+   http://www.eclipse.org/legal/epl-v10.html
+ */
+package de.belaso.mongolyn.ui;
+
+/**
+ * 
+ * @author Christian Trutz
+ * 
+ */
+public class MongolynUtils {
+
+}
